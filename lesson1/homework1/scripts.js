17c82c3bf12635d129a9ef040dc012cb7479ce67
Yura <161430612+yura-novi28@users.noreply.github.com>
2024-03-04 22:44:41 +0300

    Add files via upload

---
+++ lesson1/homework1/scripts.js
@@ -0,0 +1,22 @@
+// alert("This will help me be happy?")
+
+// Завдання 1
+let city = "Київ";
+city = "Львів";
+let address = city;
+alert(address)
+
+
+// Завдання 2
+let n = 28;
+let result = n + "³";
+console.log(result)
+
+
+// Завдання 3
+let pen = 4;
+let pricePen = 5.25;
+let pencils = 6;
+let pricePencils = 3.50;
+let sum = (pen * pricePen) + (pencils * pricePencils);
+document.write(sum)
